41bdfb21b396022a18931ac9f88c97b9a8c21f3d
leejantrix-hue <leejantrix@gmail.com>
2026-06-03 23:27:21 +0300

    feat(ui): barrel exports for ui primitives

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { Container } from './Container';
+export { Button } from './Button';
+export { Card } from './Card';
+export { Badge } from './Badge';
+export { SectionHeading } from './SectionHeading';
+export { StarRating } from './StarRating';
